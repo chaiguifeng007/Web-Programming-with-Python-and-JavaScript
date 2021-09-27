252775a100f466bd4e0fe0aad6ea60d07040f218
DESKTOP-H5ORI9G\chaiguifeng <chaiguifeng@i9i.top>
2021-09-27 11:13:32 +0800

    hello

---
+++ L2/hello.py
@@ -0,0 +1,6 @@
+print("hello world!")
+
+a = "28"
+print(a)
+
+print(a == 28)
---
+++ L2/sequences.py
@@ -0,0 +1,11 @@
+name = "Harry"
+print(name[1])
+
+names = ["Harry", "Rom", "Hermione"]
+print(names[0][0])
+
+coordinateX = 10.0
+coordinateY = 20.0
+
+coordinate = (10.0, 20.0)
+
