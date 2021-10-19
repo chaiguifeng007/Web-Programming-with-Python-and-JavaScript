73dc07dae01c3cb0de7ee8b00e9418e7ee801480
DESKTOP-H5ORI9G\chaiguifeng <chaiguifeng@i9i.top>
2021-10-19 17:43:37 +0800

    python manage.py startapp hello 重构views

---
+++ L3/lecture3/hello/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello, world")
+
+def brian(request):
+    return HttpResponse("Hello, Brian!")
+
+def david(request):
+    return HttpResponse("Hello, david")
