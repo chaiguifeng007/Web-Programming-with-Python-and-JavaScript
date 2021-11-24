c69988c5ad69648ff91ff1aacdde8d1d7a73967a
DESKTOP-89FVLAP\chaiguifeng <chaiguifeng@i9i.top>
2021-11-24 08:46:28 +0800

    airline.urls

--- L4/airline/airline/settings.py
+++ L4/airline/airline/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'flights',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'airline.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'L4',
+        'USER': 'root',
+        'PASSWORD': 'g1gjnJmwH0cKJj1A05ex',
+        'HOST': '192.168.0.124',
+        'PORT': '3306',
     }
 }
 
